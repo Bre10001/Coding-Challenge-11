58731b92a5241419370b067f758c05ddf538e120
Breanna Burris <breanna1burris@gmail.com>
2024-10-21 16:15:12 -0400

    Handle order submission and display order summary

--- main.js
+++ main.js
@@ -21,3 +21,12 @@ function calculateTotalPrice() {
     
     totalPriceElement.textContent = totalPrice.toFixed(2);
 }
+
+// Task 4:Handle order submission
+placeOrderButton.addEventListener('click', function() { //always for button to be clicked and order to be placed
+    const selectedProduct = productSelector.options[productSelector.selectedIndex].text;
+    const quantity = quantityInput.value;
+    const totalPrice = totalPriceElement.textContent;
+    
+    orderSummary.textContent = `You ordered ${quantity} of ${selectedProduct} for a total price: $${totalPrice}`; // displays the order summary showing product and total price 
+});
