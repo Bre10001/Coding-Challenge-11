7e724ce166178f3905f0389559644dc1eced76fc
Breanna Burris <breanna1burris@gmail.com>
2024-10-21 16:10:46 -0400

    Add eventlistener for productselection& quantinput

---
+++ main.js
@@ -0,0 +1,13 @@
+// Task 2: Add Event Listener for Product Selection
+
+// Define variables
+const productSelector = document.getElementById('product-selector');
+const quantityInput = document.getElementById('quantity');
+const totalPriceElement = document.getElementById('total-price');
+const placeOrderButton = document.getElementById('place-order');
+const orderSummary = document.getElementById('order-summary');
+
+
+productSelector.addEventListener('change', calculateTotalPrice);
+quantityInput.addEventListener('input', calculateTotalPrice);
+
